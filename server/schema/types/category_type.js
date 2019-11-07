91e58bc6351321686798c3354668e1a8784c7ffc
Kaneka <kane.chhak@gmail.com>
2019-11-07 09:40:02 -0800

    Create schema types for category, product, user

---
+++ server/schema/types/category_type.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
+
+const Category = mongoose.model("categories");
+
+const CategoryType = new GraphQLObjectType({
+  name: "CategoryType",
+  fields: () => ({
+    _id: { type: GraphQLID },
+    name: { type: GraphQLString }
+  })
+});
+
+modulle.exports = categoryType;
---
+++ server/schema/types/user_type.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: "UserType",
+  fields: () => ({
+    _id: {type: GraphQLID},
+    name: {type: GraphQLString},
+    email: {type: GraphQLString}
+  })
+});
+
+module.exports = UserType;

8fbb42a01e7c58efb6d2eada28302e68fac3f935
Kaneka <kane.chhak@gmail.com>
2019-11-07 13:24:38 -0800

    Create register mutation

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -1,11 +1,13 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID } = graphql;
 const mongoose = require("mongoose");
+const AuthService = require("../services/auth");
 
 const CategoryType = require("./types/category_type");
 const Category = mongoose.model("categories");
 const ProductType = require("./types/product_type");
 const Product = mongoose.model("products");
+const UserType = require("./types/user_type");
 
 const mutation = new GraphQLObjectType({
   name: "Mutation",
@@ -56,6 +58,17 @@ const mutation = new GraphQLObjectType({
       },
       resolve(_, {productId, categoryId}) {
         return Product.updateProductCategory(productId, categoryId);
+      }
+    },
+    register: {
+      type: UserType,
+      args: {
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        password: {type: GraphQLString}
+      },
+      resolve(_, args) {
+        return AuthService.register(args);
       }
     }
   }
---
+++ server/services/auth.js
@@ -0,0 +1,47 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const keys = require("../../config/keys");
+const validateRegisterInput = require("../validation/register");
+
+const register = async data => {
+  try {
+    const { message, isValid } = validateRegisterInput(data);
+
+    if (!isValid) {
+      throw new Error(message);
+    }
+
+    const {name, email, password} = data;
+
+    const existingUser = await User.findOne({email});
+
+    if (existingUser) {
+      throw new Error("This user already exists");
+    }
+
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    const user = new User(
+      {
+        name,
+        email,
+        password: hashedPass
+      },
+      err => { 
+        if (err) throw err;
+      }
+    );
+
+    user.save();
+
+    const token = jwt.sign({id: user._id}, keys.secretOrKey);
+
+    return {token, loggedIn: true, ...user._doc, password: null};
+
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {register};

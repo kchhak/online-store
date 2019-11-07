8374fdade88500f4165239655da2dd115bfac38d
Kaneka <kane.chhak@gmail.com>
2019-11-07 12:41:09 -0800

    Create mutations to add and delete product, category, updateProductCategory

--- server/models/Product.js
+++ server/models/Product.js
@@ -24,4 +24,27 @@ const ProductSchema = new Schema({
   }
 });
 
+ProductSchema.statics.updateProductCategory = (productId, categoryId) => {
+  const Product = mongoose.model("products");
+  const category = mongoose.model("categories");
+
+  return Product.findById(productId).then(product => {
+    if (product.category) {
+      category.findById(product.category).then(oldcategory => {
+        oldcategory.products.pull(product);
+        return oldcategory.save();
+      });
+    }
+
+    return category.findById(categoryId).then(newCategory => {
+      product.category = newCategory;
+      newCategory.products.push(product);
+
+      return Promise.all([product.save(), newCategory.save()]).then(
+        ([product, newCategory]) => product
+      );
+    });
+  });
+};
+
 module.exports = mongoose.model("products", ProductSchema);
--- server/schema/types/category_type.js
+++ server/schema/types/category_type.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLList } = graphql;
 
 const Category = mongoose.model("categories");
 
@@ -8,7 +8,13 @@ const CategoryType = new GraphQLObjectType({
   name: "CategoryType",
   fields: () => ({
     _id: { type: GraphQLID },
-    name: { type: GraphQLString }
+    name: { type: GraphQLString },
+    products: {
+      type: new GraphQLList(require("./product_type")),
+      resolve(category) {
+        return Category.findProducts(category._id);
+      }   
+    }
   })
 });
 
